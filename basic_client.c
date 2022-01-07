d6110bfe24ecfb82298fe912ddce194a2a42ac19
Macy Jiang <52924027+mjiang97@users.noreply.github.com>
2022-01-06 20:44:35 -0500

    updated files

--- basic_client.c
+++ basic_client.c
@@ -1,16 +1,13 @@
 #include "pipe_networking.h"
 
 int main() {
-    int to_server;
-    int from_server;
-
-    from_server = client_connect( &to_server );
     char line[BUFFER_SIZE];
+    int sd = client_connect();
     while(1) {
         printf("input: ");
         fgets(line, sizeof(line), stdin);
-        write(to_server, line, sizeof(line));
-        read(from_server, line, sizeof(line));
+        write(sd, line, sizeof(line));
+        read(sd, line, sizeof(line));
         printf("response: %s", line);
     }
 }
--- basic_server.c
+++ basic_server.c
@@ -2,7 +2,7 @@
 
 char *toUpper(char line[]) {
     char * input = line;
-    int i = 0;
+    int i;
     // process input str into all caps
     for (i = 0; i < strlen(line); i++) {
         line[i] = toupper(line[i]);
@@ -11,22 +11,20 @@ char *toUpper(char line[]) {
 }
 
 int main() {
-    int to_client;
-    int from_client;
-
+    int sd = server_setup();
     while(1) {
-        from_client = server_setup();
+        int from_client = server_connect(sd);
         int f = fork();
         if(f) {
             close(from_client);
         } else {
-            to_client = server_connect(from_client);
+            char message[BUFFER_SIZE];
             while(1) {
                 char message[BUFFER_SIZE];
                 int r = read(from_client, message, sizeof(message));
                 if(r==0) break;
                 toUpper(message);
-                write(to_client, message, sizeof(message));
+                write(from_client, message, sizeof(message));
             }
         }
     }
